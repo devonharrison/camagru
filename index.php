338a62d466cc24bacbe1f2e7d619bd87abc875a9
lisa HARRISON <lharris@c3r5s3.wethinkcode.co.za>
2018-10-17 15:56:32 +0200

    initial

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php?>
+
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+<title>Camagru</title>
+<link rel="stylesheet" href="home.css">
+</head>
+
+<body>
+<div id="home">
+    <div id="left"></div>
+    <form class="register"></form>
+</div>
+</body>
