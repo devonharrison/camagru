52239ca00c88160e02a64a06b0d0453149f3a604
lisa HARRISON <lharris@c3r5s3.wethinkcode.co.za>
2018-10-25 17:50:40 +0200

    nm

--- login.php
+++ login.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en-US">
     <head>
-        <title>Camagru</title>
+        <title>Login</title>
         <link rel="stylesheet" href="login.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
-<body id="body">
-    <div id="main">
+<body>
+    <div>
         <center><h2>Login</h2></center>
      <form class="form" action="login.php" method="post">
         <label>Username:</label><br>
