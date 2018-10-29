193b6dd02af408da44ef3dd433187daea079cbcd
lisa HARRISON <lharris@c3r5s3.wethinkcode.co.za>
2018-10-29 14:11:56 +0200

    vp

--- login.php
+++ login.php
@@ -3,7 +3,8 @@
     <head>
         <title>Login</title>
         <link rel="stylesheet" href="login.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 
 <body>
--- signup.php
+++ signup.php
@@ -3,6 +3,8 @@
 <head>
     <title>Register</title>
     <link rel="stylesheet" href="signup.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
     <center><h1>Camagru</h1></center>
