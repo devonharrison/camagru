60fc60f53c9903c28aa81e941e56a22cc5bd93bb
Devon HARRISON <dharriso@c3r5s4.wethinkcode.co.za>
2018-10-29 14:11:38 +0200

    login kinda working, not really

--- login.php
+++ login.php
@@ -18,5 +18,47 @@
             <button type="submit" id="login_btn" name="login">Login</button>
         </form>
     </div>
+    <?php
+          session_start();
+          $servername = "localhost";
+          $dusername = "root";
+          $password = "password";
+          $dbname = "camagru";
+          $conn = mysqli_connect($servername, $dusername, $password, $dbname);
+          if (!$conn)
+          {
+              die("Connection failed: " . mysqli_connect_error()."<br>");
+          }
+            if (isset($_POST['login']))
+            {
+                if (empty($_POST['username']) || empty($_POST['password']))
+                {
+                    echo "Empty field <br>";
+                }
+                else
+                {
+                    $username = $_POST['username'];
+                    $password_1 = $_POST['password'];
+                    $qry = "SELECT username, password FROM users";
+                    $result = mysqli_query($conn, $qry);
+                    $u = 0;
+                    if (mysqli_num_rows($result) > 0)
+                    {
+                        while ($row = mysqli_fetch_assoc($result))
+                        {
+                            if ($row['username'] == $username)
+                            {
+                                $pw = $row['password'];
+                                if (password_verify($password_1, $pw) == TRUE)
+                                {
+                                    header('Location: http://localhost:8080/camagru/home.php');
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+            }
+    ?>
 </body>
 </html>
