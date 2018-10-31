093baadb663d2c08dc2cadf12a2bb115f0902a5e
Devon HARRISON <dharriso@c3r5s4.wethinkcode.co.za>
2018-10-31 11:31:25 +0200

    forgotpw

--- forgotpw.php
+++ forgotpw.php
@@ -11,12 +11,47 @@
     <div>
         <center><h2>Forgot password</h2></center>
         <form class="form" action="login.php" method="post">
-            <label>Username:</label><br>
-            <input type="text" name="username" class="inputvalues" placeholder="Enter a username"/><br>
-            <label>Password:</label><br>
-            <input type="password" name="password" class="inputvalues" placeholder="Enter a password"/><br>
-            <button type="submit" id="login_btn" name="login">Login</button>
+            <label>Email:</label><br>
+            <input type="text" name="email" class="inputvalues" placeholder="Confirm email address"/><br>
+            <button type="submit" id="login_btn" name="send">Send email</button>
         </form>
     </div>
+    <?php
+          $servername = "localhost";
+          $dusername = "root";
+          $password = "password";
+          $dbname = "camagru";
+          $conn = mysqli_connect($servername, $dusername, $password, $dbname);
+          if (!$conn)
+          {
+              die("Connection failed: " . mysqli_connect_error()."<br>");
+          }
+            if (isset($_POST['send']))
+            {
+                if (empty($_POST['email']))
+                {
+                    echo "Empty field <br>";
+                }
+                else
+                {
+                    $email = $_POST['email'];
+                    $qry = "SELECT email FROM users";
+                    $result = mysqli_query($conn, $qry);
+                    if (mysqli_num_rows($result) > 0)
+                    {
+                        while ($row = mysqli_fetch_assoc($result))
+                        {
+                            if ($row['email'] == $email)
+                            {
+                                $subject = "Camagru password change";
+                                $body = "http://localhost:8080/camagru/changepw.php";
+                                $headers = "From: noreply@camagru.com";
+                                mail ($email, $subject, $body, $headers);
+                            }
+                        }
+                    }
+                }
+            }
+    ?>
 </body>
 </html>
