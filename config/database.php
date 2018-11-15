e60ffd3be8d3cad072aa4fd19c02b1e04f41c305
Devon HARRISON <dharriso@c3r5s4.wethinkcode.co.za>
2018-11-15 15:46:14 +0200

    database and table creation PDO

--- config/database.php
+++ config/database.php
@@ -11,82 +11,58 @@ $tbl_users = "users";
 /*
 Create connection
 */
-$conn = mysqli_connect($servername, $dusername, $password);
-/*
-Check connection
-*/
-if (!$conn)
+try 
 {
-    die("Connection failed: " . mysqli_connect_error()."<br>");
+    $conn = new PDO("mysql:host=$servername", $dusername, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);;
+    echo "[INFO] Connection successful<br>";
 }
-
-/* 
-Create database if it hasn't been already
-*/
-$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
-if (mysqli_query($conn, $sql))
+catch(PDOException $e)
 {
-    echo "Database created successfully<br>";
+    echo "[INFO] " . $e->getMessage() . "<br>";
 }
-else
+/* create databse */
+try
 {
-    echo "Error creating database: " . mysqli_error($conn)."<br>";
+    $sql = "CREATE DATABASE $dbname";
+    $conn->exec($sql);
+    echo "[INFO] Database creation succesful<br>";
 }
-
-/*
-Refresh connection
-*/
-$conn = mysqli_connect($servername, $dusername, $password, $dbname);
-if (!$conn)
+catch(PDOException $e)
 {
-    die("Connection failed: " . mysqli_connect_error()."<br>");
+    echo "[INFO] Database creation failed " . $e->getMessage() . "<br>";
 }
-
-/*
-User Table
-*/
-$sql = "CREATE TABLE IF NOT EXISTS $tbl_users (
-	user_id INT(100) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-	name TEXT NOT NULL, surname TEXT NOT NULL, username TEXT NOT NULL,
-    email TEXT NOT NULL, password TEXT NOT NULL, verified TEXT NOT NULL)";
-	
-if (mysqli_query($conn, $sql)) {
-	echo "User Table created successfully<br>";
-} else {
-	echo "Error creating table: ".mysqli_error($conn)."<br>";
+/* Create tables */
+try
+{
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $dusername, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $users = "CREATE TABLE IF NOT EXISTS $tbl_users (
+        user_id INT(100) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, 
+        name TEXT NOT NULL, surname TEXT NOT NULL, username TEXT NOT NULL,
+        email TEXT NOT NULL, password TEXT NOT NULL, verified TEXT NOT NULL)";
+    $conn->exec($users);
+    echo "[INFO] User table created successfully<br>";
+    /* Image table */
+    $images = "CREATE TABLE IF NOT EXISTS images (  
+        id int(11) NOT NULL AUTO_INCREMENT,  
+        name blob NOT NULL,  
+        PRIMARY KEY (id)  
+    ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ";
+    $conn->exec($images);
+    echo "[INFO] Image table created successfully<br>";
+    /*Comments table */
+    $comments = "CREATE TABLE IF NOT EXISTS comments (
+        id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        uid VARCHAR(128) NOT NULL,
+        date VARCHAR(128) NOT NULL,
+        message TEXT NOT NULL)";
+    $conn->exec($comments);
+    echo "[INFO] Comment table creation successful<br>";
 }
-
-/*
-image table
-*/
-
-$sql1 = "CREATE TABLE IF NOT EXISTS images (  
-     id int(11) NOT NULL AUTO_INCREMENT,  
-     name blob NOT NULL,  
-     PRIMARY KEY (id)  
- ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ";
-   if (mysqli_query($conn, $sql1))
-   {
-       echo "Image Table created successfully<br>";
-    }
-    else
-    {
-        echo "Error creating table: ".mysqli_error($conn)."<br>";
-    }
-
-$sql2 = "CREATE TABLE IF NOT EXISTS comments (
-    id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    uid VARCHAR(128) NOT NULL,
-    date VARCHAR(128) NOT NULL,
-    message TEXT NOT NULL
-    )";
-    if (mysqli_query($conn, $sql2))
-    {
-        echo "Comment Table created successfully<br>";
-    }
-    else
-    {
-        echo "Error creating table: ".mysqli_error($conn)."<br>";
-    }
-mysqli_close($conn);
+catch(PDOException $e)
+{
+    echo "[INFO] " . $e->getMessage();
+}
+$conn = null;
 ?>
