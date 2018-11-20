b73ba241e541a8cf3cb22f6ece5f04bbfc15fdaa
Itumeleng LETIMILE <iletimil@c1r4s6.wethinkcode.co.za>
2018-11-20 15:50:36 +0200

    dell

--- webcam/storeimage.php
+++ webcam/storeimage.php
@@ -5,19 +5,7 @@
     $password = "password";
     $dbname = "camagru";
     $name = "";
-    // $folderPath = "../upload/";
-  
-    // $image_parts = explode(";base64,", $img);
-    // $image_type_aux = explode("image/", $image_parts[0]);
-    // $image_type = $image_type_aux[1];
-  
-    // $image_base64 = base64_decode($image_parts[1]);
-    // $fileName = uniqid() . '.png';
-  
-    // $file = $folderPath . $fileName;
-    // file_put_contents($file, $image_base64);
-  
-    // print_r($fileName);
+    
     try
     {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $dusername, $password);
