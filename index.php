f2884c994ff1f101580812d848f6c2a19c56d7c4
lisa HARRISON <lharris@c3r5s5.wethinkcode.co.za>
2018-11-20 12:36:31 +0200

    still funky

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
-<?php
+
+
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <title>Camagru</title>
+    <link rel="stylesheet" href="index.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+
+    <div class="top">
+        <a class="camagru" href="#home">Camagru</a>
+        <a class="info" href="login.php" name='login' >Login</a>
+        <a class="info" href="signup.php" name='signup'>Sign-up</a>
+    </div>
+
+    <div class="container">
+        <div class="row">
+    <?php
     $servername = "localhost";
     $dusername = "root";
     $password = "password";
@@ -23,38 +45,7 @@
             echo "[INFO] " . $e->getMessage();
         }
 ?>
-
-<!DOCTYPE html>
-<html lang="en-US">
-
-<head>
-    <title>Camagru</title>
-    <link rel="stylesheet" href="index.css">
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-</head>
-
-<body>
-
-    <div class="top">
-        <a class="camagru" href="#home">Camagru</a>
-        <a class="info" href="login.php" name='login' >Login</a>
-        <a class="info" href="signup.php" name='signup'>Sign-up</a>
-    </div>
-
-    <div class="container">
-<<<<<<< HEAD
-            
-=======
-        <div class="row">
-            <div class="column"></div>
-            <div class="column"></div>
-            <div class="column"></div>
-            <div class="column"></div>
-            <div class="column"></div>
-            <div class="column"></div>
-        </div>
->>>>>>> 2e66e7700cc8205171dd8e1c47a923a7ccdccc2e
+</div>
     </div>
 
     <?php
