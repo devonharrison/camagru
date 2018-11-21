70bc24814d82f806f62a6214368c92f20cbed5b2
lisa HARRISON <lharris@c3r5s5.wethinkcode.co.za>
2018-11-21 14:15:59 +0200

    TUMI

---
+++ picture.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <title>Camagru</title>
+    <link rel="stylesheet" href="home.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+    <div class="top">
+        <a class="camagru" href="home.php">Camagru</a>
+        <a class="info" href="index.php" name='logout'>Logout</a>
+        <a class="info" href="profile.php" name='signup'>Profile</a>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <?php
+            
+            ?>
+        </div>
+    </div>
+</body>
+</html>
