35d7d3b6707aa4fc4d84f3726df89a0aa45eb3de
Devon HARRISON <dharriso@c3r5s4.wethinkcode.co.za>
2018-11-26 18:41:06 +0200

    delete image work in progress

---
+++ deleteimg.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <title>Camagru</title>
+    <link rel="stylesheet" href="index.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+    <div class="header">
+        <a class="camagru" href="home.php">Camagru</a>
+        <div class="header-right">
+            <a class="info" href="index.php" name='logout'>Logout</a>
+            <a class="info" href="profile.php" name='signup'>Profile</a>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="column">
+            <?php
+            
+            ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
